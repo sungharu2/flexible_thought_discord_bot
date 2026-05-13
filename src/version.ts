0652be52c3de2f4e291c34661b483d538e1063a8
sungharu <38002442+sungharu2@users.noreply.github.com>
2026-05-13 19:08:18 +0900

    version: v0.4.0 배포

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 export const version = {
-    versionCode: 'v0.3.1',
-    lastUpdateDate: '2026-05-12',
+    versionCode: 'v0.4.0',
+    lastUpdateDate: '2026-05-13',
     developerUserId: '287544461381074946'
 }
